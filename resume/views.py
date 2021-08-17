14e196c4ad4a32632adbefd27043cfe33c21c268
sparkle6 <sparkdkiller@gmail.com>
2021-08-17 17:43:03 +0100

    Added a few files

--- resume/views.py
+++ resume/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, redirect
 from .forms import BioForm, SkillsForm
+from .models import Bio
 # Create your views here.
 
 def index(request):
+  if request.method == "POST":
+    form = BioForm(request.POST)
+    if form.is_valid():
+      form.save()
+      print("Form saved..")
+      return redirect("index")
   form = BioForm()
   context = {"form" : form}
   return render(request, "resume/index.html", context )
